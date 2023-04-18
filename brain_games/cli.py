b2901fa122a9e0e6c70e72622b7a65eb72fc6f7b
Igor Loskutov <omfi017@gmail.com>
2023-04-19 01:19:12 +0500

    add common functions in cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -8,3 +8,29 @@ def welcome_user():
     print("Hello, " + name + "!")
 
     return name
+
+
+def ask(task):
+
+    print(f"Question: {task}")
+
+
+def get_answer():
+
+    answer = prompt.string("Your answer: ")
+    return answer
+
+
+def compare(correct_answer, answer):
+
+    if(correct_answer == answer):
+        print("Correct!")
+        return True
+    else:
+        print(f"\'{answer}\' is wrong answer ;(. Correct answer was \'{correct_answer}\'")
+        return False
+
+
+def congrats(name):
+
+    print(f"Congratulations, {name}!")
