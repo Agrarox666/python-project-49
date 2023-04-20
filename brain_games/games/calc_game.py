b722f044bb1f98dd07c734234932103e88bc4bab
Igor Loskutov <omfi017@gmail.com>
2023-04-20 14:29:48 +0500

    fix some code-climate problems in calc-game

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-from random import randint
+from random import randint, choice
 from brain_games.cli import ask, get_answer, compare
 
 
 def calc_game():
 
+    sign = ['+', '-', '*']
     i = 0
     while (i < 3):
 
         num1 = randint(0, 100)
         num2 = randint(0, 100)
-        sign = random.choice(sign)
+        sign = choice(sign)
 
         ask(make_expression(num1, num2, sign))
 
@@ -20,22 +21,19 @@ def calc_game():
             print("Error!")
             return False
 
-        correct_answer = calc(num1, num2, sign)
-
-        if compare(correct_answer, answer):
+        if compare(find_answer(num1, num2, sign), answer):
             i += 1
         else:
             return False
     return True
 
 
-def make_expression(a, b, sign_num):
+def make_expression(a, b, sign):
 
-    sign = ['+', '-', '*']
-    return f"{a} {random.choice(sign)} {b}"
+    return f"{a} {sign} {b}"
 
 
-def calc(a, b, sign):
+def find_answer(a, b, sign):
 
     if sign == 0:
         return a + b
