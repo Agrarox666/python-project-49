c03345f271d0e9907f214be9761ca7c18a525ee2
Igor Loskutov <omfi017@gmail.com>
2023-04-20 19:35:14 +0500

    add game.py with common games logic

---
+++ brain_games/game.py
@@ -0,0 +1,62 @@
+import prompt
+
+def run_game(game_task, game_solution, game_description):
+
+    name = welcome_user()
+    print(game_description)
+
+    x = 0
+    while (x < 3):
+
+        task = game_task()
+        ask(task)
+        answer = get_answer()
+
+        if (is_correct(game_solution(task), answer)):
+            x += 1
+        else:
+            consolation(name)
+            return False
+
+    congrats(name)
+    return True
+
+
+def welcome_user():
+
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + name + "!")
+
+    return name
+
+
+def ask(task):
+
+    print(f"Question: {task}")
+
+
+def get_answer():
+
+    answer = prompt.string("Your answer: ")
+    return answer
+
+
+def is_correct(correct_answer, answer):
+
+    if (correct_answer == answer):
+        print("Correct!")
+        return True
+    else:
+        print(f"\'{answer}\' is wrong answer ;(. "
+              f"Correct answer was \'{correct_answer}\'")
+        return False
+
+def congrats(name):
+
+    print(f"Congratulations, {name}!")
+
+
+def consolation(name):
+
+    print(f"Let's try again, {name}!")
