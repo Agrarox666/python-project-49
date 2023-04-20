af7f4b2737e447eda0e298f1cd25d9d405be3434
Igor Loskutov <omfi017@gmail.com>
2023-04-20 14:36:01 +0500

    fix bugs in calc-game

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -3,15 +3,17 @@ from random import randint, choice
 from brain_games.cli import ask, get_answer, compare
 
 
+SIGN = ['+', '-', '*']
+
+
 def calc_game():
 
-    sign = ['+', '-', '*']
     i = 0
     while (i < 3):
 
         num1 = randint(0, 100)
         num2 = randint(0, 100)
-        sign = choice(sign)
+        sign = choice(SIGN)
 
         ask(make_expression(num1, num2, sign))
 
@@ -35,9 +37,9 @@ def make_expression(a, b, sign):
 
 def find_answer(a, b, sign):
 
-    if sign == 0:
+    if sign == '+':
         return a + b
-    elif sign == 1:
+    elif sign == '-':
         return a - b
     else:
         return a * b
