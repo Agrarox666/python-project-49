61e31c81379f47e85ba2eef3259496bd97ffe27f
Igor Loskutov <omfi017@gmail.com>
2023-04-20 14:40:30 +0500

    fix some code-climate problems in gcd-game

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -5,22 +5,20 @@ from brain_games.cli import ask, get_answer, compare
 
 def gcd_game():
 
-    print("Find the greatest common divisor of given numbers.")
     i = 0
     while (i < 3):
 
         random_number1 = randint(0, 100)
         random_number2 = randint(0, 100)
-        correct_answer = find_gcd(random_number1, random_number2)
-
         ask(f"{random_number1} {random_number2}")
+
         try:
             answer = int(get_answer())
         except ValueError:
             print("Error!")
             return False
 
-        if compare(correct_answer, answer):
+        if compare(find_gcd(random_number1, random_number2), answer):
             i += 1
         else:
             return False
